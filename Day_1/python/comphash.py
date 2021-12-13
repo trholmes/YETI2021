4ab4361227b2b7f5e9b8ac5df0b6598870eccefb
James Christie <mattacusspartacus@gmail.com>
2021-12-13 17:58:58 -0500

    Cleanup and comments on python

--- Day_1/python/comphash.py
+++ Day_1/python/comphash.py
@@ -5,7 +5,7 @@ import re
 # pool of characters to search
 charpool : str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-# target hash
+# get the target hash from the file
 target = ""
 with open("../data/targethash.txt", "r") as fp:
     target = fp.readlines()[0].replace("\n","")
@@ -25,15 +25,18 @@ tinylink = f'https://tiny.utk.edu/{result}'
 # we'll get the final url, to get a bit of info about where the data comes from
 urlafterredirect = urllib.request.urlopen(tinylink).geturl()
 print(urlafterredirect)
+
 # get the html from the webpage and save it
 sitehtml = urllib.request.urlretrieve(tinylink, "../data/htmlresult.html")
+
 # open that as a string, saves a bit of encode decode headache
 sitehtmlstr = ''
 with open("../data/htmlresult.html", "r") as fp:
     sitehtmlstr = ".".join(fp.readlines())
+
 # seems it's a gist, regex to find the raw path
 downloadpath = urlafterredirect + "/" + re.findall("href=.+/(raw.+?)\"", sitehtmlstr)[0]
 
 print(downloadpath)
-# download and save the data
+# download and save the data, using the path we found above
 rawdat = urllib.request.urlretrieve(downloadpath, "../data/FourVectorTest.csv")
