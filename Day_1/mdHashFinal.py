25e4cfd6f6fc563b64d45cc5691949d138f8247e
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-13 18:42:40 -0500

    Update mdHashFinal.py
    Commented code

--- Day_1/mdHashFinal.py
+++ Day_1/mdHashFinal.py
@@ -7,5 +7,6 @@ hashStr = lambda plaintxt: hsh.md5(plaintxt.encode()).hexdigest()
 #makes it so you can compare the values
 getptx = lambda hash: next((plain for plain in plaintxt if hashStr(plain) == hash),None) 
 #returns string corresponding to given mdhash and if there are none, returns None
+#as md5 hash is injective, only one string results in the desired hash
 print(getptx("4002f685108db38f1af9965f3bc869eb"))
 
