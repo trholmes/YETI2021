f3e95269c33a846a2848e5f4627d060ca775ba3a
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-16 05:19:13 -0500

    Add files via upload

--- Day_2/fourVectorDay2.py
+++ Day_2/fourVectorDay2.py
@@ -7,17 +7,19 @@ azimuthal = np.zeros(100000)
 fourVectors=[]
 f = open('FourVectorTest.csv')
 csv_reader=csv.reader(f)
-pseudorap = lambda px, py, pz: np.arctanh(pz/(np.sqrt(px**2+py**2+pz**2)))
-#function for pseudorapidity
-azim = lambda px, py: np.arctan2(py,px)
-#returns azimuthal angle
+
+pseudorap=lambda px,py,pz: np.arctanh(pz/(np.sqrt(px**2+py**2+pz**2)))
+#function for pseudorapidity: arctanh(pz/p)
+
 for lineNumber,line in enumerate(csv_reader,1):
-    pseudoRap[lineNumber-1] = pseudorap(float(line[0]),float(line[1]),float(line[2]))
-    azimuthal[lineNumber-1] = azim(float(line[0]),float(line[1]))
-    fourVectors.append(Momentum4(line[3],line[0],line[1],line[2]))
+    px, py, pz, E = float(line[0]), float(line[1]),float(line[2]), float(line[3])
+    pseudoRap[lineNumber-1] = pseudorap(px,py,pz)
+    azimuthal[lineNumber-1] = (lambda x, y:np.arctan2(x,y))(py, px)
+    #azimuthal angle is arctan2(py/px)
+    fourVectors.append(Momentum4(E,px,py,pz))
     #creates list of four momenta (E,px,py,pz)
 f.close()
-plt.hist2d(pseudoRap,azimuthal,bins=250, cmap='viridis', norm=LogNorm())
+plt.hist2d(pseudoRap,azimuthal,bins=250, cmap='cool', norm=LogNorm())
 plt.colorbar()
 plt.title('2D Histogram of Pseudorapidity vs Azimuthal Angle (aka Knoxville)')
 plt.xlabel(r'Pseudorapidity $\eta$')
