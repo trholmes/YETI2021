ee4b085cab865684449ea948e73502a2d490488a
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-16 05:39:53 -0500

    Add files via upload
    More readable code

--- Day_1/mdhash5Final.py
+++ Day_1/mdhash5Final.py
@@ -1,9 +1,12 @@
 import hashlib as hsh, itertools as it, string
-checkHash = lambda plain, hash: hsh.md5(plain.encode()).hexdigest() == hash
+def checkHash(hash, plaintxt): return hsh.md5(plaintxt.encode()).hexdigest() == hash
 #checks the hash of plain against the inputted hash
-plaintxt = lambda chars,length: map(''.join, it.product(chars, repeat=length))
-#generates all strings created from chars with the given length
-getPTX = lambda hash, chars, length: next((plain for plain in plaintxt(chars,length) if checkHash(plain, hash) == True),None)
-#returns string made from chars with given length that has the desired hash, if there are none returns none 
+def ptxt(chars,length): return map(''.join, it.product(chars, repeat=length))
+#generates all strings created from list chars with the given length
+def getPTX(hash, chars): 
+    decode = next((string for string in ptxt(chars,4) if checkHash(hash,string) == True),None)
+    return decode
+#returns string from plaintxt(chars,length) that has the desired hash, if there are none returns none 
 #rests on injectivity of md5hash
-print(getPTX("4002f685108db38f1af9965f3bc869eb", string.ascii_uppercase+string.digits+string.ascii_lowercase,4))
+print(getPTX("4002f685108db38f1af9965f3bc869eb", string.ascii_uppercase+string.digits+string.ascii_lowercase))
+
