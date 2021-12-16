9fa00e337ff293fa44d8090e8aeb4066b130e233
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-16 05:41:04 -0500

    Add files via upload

--- Day_3/primeNumbers.py
+++ Day_3/primeNumbers.py
@@ -7,10 +7,9 @@ Created on Wed Dec 15 12:31:03 2021
 
 from primesieve import *
 primenums = n_primes(500)
-#first 500 prime numbers
-#primesieve uses C to generate list of primes, it works faster than the sieve I coded
-sumPrimes = lambda idx, seqLen: sum(primenums[idx:idx+seqLen])
-#idx is the index of the number to start summing from, seqLen is the 
+#primesieve uses C to generate list of primes, it works faster than the sieve I tried to code
+def sumPrimes(ind,seqLen): return sum(primenums[ind:ind+seqLen])
+#index is the element to start summing from, primes is the list of primes, seqLen is the 
 #number of consecutive primes
 sumsOfThree = [sumPrimes(i, 3) for i in range(497)]
 sumsOfThreeAndNine = [sumPrimes(i,9) for i in range(492) if sumPrimes(i,9) in sumsOfThree]
