db5229789c99f2d160cc85b3468731b77a403d2a
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-16 05:33:58 -0500

    Add files via upload
    Shortened/more readable code

--- Day_2/fourVectorDay2.py
+++ Day_2/fourVectorDay2.py
@@ -8,18 +8,17 @@ fourVectors=[]
 f = open('FourVectorTest.csv')
 csv_reader=csv.reader(f)
 
-pseudorap=lambda px,py,pz: np.arctanh(pz/(np.sqrt(px**2+py**2+pz**2)))
-#function for pseudorapidity: arctanh(pz/p)
 
 for lineNumber,line in enumerate(csv_reader,1):
     px, py, pz, E = float(line[0]), float(line[1]),float(line[2]), float(line[3])
-    pseudoRap[lineNumber-1] = pseudorap(px,py,pz)
-    azimuthal[lineNumber-1] = (lambda x, y:np.arctan2(x,y))(py, px)
+    pseudoRap[lineNumber-1] = np.arctanh(pz/(np.sqrt(px**2+py**2+pz**2)))
+    #eta is arctanh(pz/p)
+    azimuthal[lineNumber-1] = np.arctan2(py,px)
     #azimuthal angle is arctan2(py/px)
     fourVectors.append(Momentum4(E,px,py,pz))
     #creates list of four momenta (E,px,py,pz)
 f.close()
-plt.hist2d(pseudoRap,azimuthal,bins=250, cmap='cool', norm=LogNorm())
+plt.hist2d(pseudoRap,azimuthal,bins=250, cmap='winter', norm=LogNorm())
 plt.colorbar()
 plt.title('2D Histogram of Pseudorapidity vs Azimuthal Angle (aka Knoxville)')
 plt.xlabel(r'Pseudorapidity $\eta$')
