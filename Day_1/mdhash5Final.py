8946fbafb17dbfb101f6923473e74ae64e6b275a
hradhakrishnan100 <95509034+hradhakrishnan100@users.noreply.github.com>
2021-12-16 05:44:34 -0500

    Add files via upload

--- Day_1/mdhash5Final.py
+++ Day_1/mdhash5Final.py
@@ -3,10 +3,11 @@ def checkHash(hash, plaintxt): return hsh.md5(plaintxt.encode()).hexdigest() == 
 #checks the hash of plain against the inputted hash
 def ptxt(chars,length): return map(''.join, it.product(chars, repeat=length))
 #generates all strings created from list chars with the given length
-def getPTX(hash, chars): 
+def getPTX(hash): 
+    chars = string.ascii_uppercase+string.digits+string.ascii_lowercase
     decode = next((string for string in ptxt(chars,4) if checkHash(hash,string) == True),None)
     return decode
 #returns string from plaintxt(chars,length) that has the desired hash, if there are none returns none 
 #rests on injectivity of md5hash
-print(getPTX("4002f685108db38f1af9965f3bc869eb", string.ascii_uppercase+string.digits+string.ascii_lowercase))
+print(getPTX("4002f685108db38f1af9965f3bc869eb"))
 
