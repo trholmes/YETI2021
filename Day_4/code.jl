bfd7f3a7e2ecc439078d6ab545699a9207327a7d
Casey Sobecks <csobe126@gmail.com>
2021-12-17 01:18:15 -0500

    Final update for Day4 code

--- Day_4/code.jl
+++ Day_4/code.jl
@@ -1,5 +1,34 @@
 
-#vigenere forward function given a code.
+#vigenere decryption
+function decryption(code)
+    key=["B","I","J","O","U"]
+    cleancode=split(uppercase(code),"")
+    cleankey=repeat(key,length(cleancode))[1:length(cleancode)]
+    a=zeros(length(cleancode))
+    b=zeros(length(cleancode))
+    c=zeros(length(cleancode))
+    solution=zeros(length(cleancode))
+    decoded=zeros(length(cleancode))
+    @threads for i in 1:length(cleancode)
+        a[i]=parse(Int,cleancode[i],base=36)
+        b[i]=parse(Int,cleankey[i],base=36)
+        a[i]+=10
+        if a[i]>35
+            a[i]-=36
+        end
+        decoded[i]=(abs(a[i]-b[i]+36))%36
+    end
+    c=Int.(decoded)
+    solution=Array{String}(undef,length(c))
+    @threads for i in 1:length(c)
+        solution[i]=string(c[i],base=36)
+    end
+    solution=join(solution)
+    solution
+end
+
+
+#vigenere encryption function
 function encrypt(code)
     cleancode=split(uppercase(code),"")
     key=["B","I","J","O","U"]
@@ -13,37 +42,10 @@ function encrypt(code)
         a[i]=parse(Int,cleancode[i],base=36)
         b[i]=parse(Int,cleankey[i],base=36)
         encoded[i]=abs(a[i]+b[i]+36)%36
-        encoded[i]+=10
-        if encoded[i]>35
-            encoded[i]-=36
+        encoded[i]-=10
+        if encoded[i]<0
+            encoded[i]+=36
         end        
-    end
-    c=Int.(encoded)
-    solution=Array{String}(undef,length(c))
-    @threads for i in 1:length(c)
-        solution[i]=string(c[i],base=36)
-    end
-    solution=join(solution)
-    solution
-end
-
-function decrypt(code)
-    cleancode=split(uppercase(code),"")
-    key=["B","I","J","O","U"]
-    cleankey=repeat(key,length(cleancode))[1:length(cleancode)]
-    a=zeros(length(cleancode))
-    b=zeros(length(cleancode))
-    c=zeros(length(cleancode))
-    encoded=zeros(length(cleancode))
-    solution=zeros(length(cleancode))
-    @threads for i in 1:length(cleancode)
-        a[i]=parse(Int,cleancode[i],base=36)
-        b[i]=parse(Int,cleankey[i],base=36)
-        a[i]-=10
-        if a[i]<0
-            a[i]+=36
-        end
-        encoded[i]=abs(a[i]-b[i]+36)%36        
     end
     c=Int.(encoded)
     solution=Array{String}(undef,length(c))
