3cc3449b52c896b069526dcb57d065f5b732d82d
Casey Sobecks <csobe126@gmail.com>
2021-12-17 12:00:57 -0500

    Fixes to decoding
    correction to decoding

--- Day_4/code.jl
+++ Day_4/code.jl
@@ -1,57 +1,52 @@
-
-#vigenere decryption
-function decryption(code)
-    key=["B","I","J","O","U"]
-    cleancode=split(uppercase(code),"")
-    cleankey=repeat(key,length(cleancode))[1:length(cleancode)]
-    a=zeros(length(cleancode))
-    b=zeros(length(cleancode))
-    c=zeros(length(cleancode))
-    solution=zeros(length(cleancode))
-    decoded=zeros(length(cleancode))
-    @threads for i in 1:length(cleancode)
-        a[i]=parse(Int,cleancode[i],base=36)
-        b[i]=parse(Int,cleankey[i],base=36)
-        a[i]+=10
-        if a[i]>35
-            a[i]-=36
-        end
-        decoded[i]=(abs(a[i]-b[i]+36))%36
+function vigenere()
+    codex=Array{Any}(undef,36,2)
+    for i in 0:25
+        codex[i+1,1]=uppercase(string(i+10,base=36))
+        codex[i+1,2]=i
     end
-    c=Int.(decoded)
-    solution=Array{String}(undef,length(c))
-    @threads for i in 1:length(c)
-        solution[i]=string(c[i],base=36)
+    for i in 0:9
+        codex[i+27,1]=string(i)
+        codex[i+27,2]=i+26
     end
-    solution=join(solution)
-    solution
+    codex
 end
 
+function decrypt(code,codex)
+    key=[["B",0],["I",0],["J",0],["O",0],["U",0]]
+    scode=split(uppercase(code),"")
+    
+    @threads for i in 1:length(key)
+        @threads for j in 1:length(codex)
+            if codex[j,1]==key[i][1]
+                key[i][2]=codex[j,2]
+                break
+            end
+        end
+    end
+    skey=repeat(key,length(scode))[1:length(scode)]
+    ncode=zeros(length(scode))
+    @threads for i in 1:length(scode)
+        @threads for j in 1:length(codex)
+            if codex[j,1]==scode[i]
+                ncode[i]=codex[j,2]
+                break
+            end
+        end
+    end
+    
+    decoded=zeros(length(ncode))
+    @threads for i in 1:length(ncode)
+        @inbounds decoded[i]=(abs(ncode[i]+skey[i][2])+36)%36
+    end
 
-#vigenere encryption function
-function encrypt(code)
-    cleancode=split(uppercase(code),"")
-    key=["B","I","J","O","U"]
-    cleankey=repeat(key,length(cleancode))[1:length(cleancode)]
-    a=zeros(length(cleancode))
-    b=zeros(length(cleancode))
-    c=zeros(length(cleancode))
-    encoded=zeros(length(cleancode))
-    solution=zeros(length(cleancode))
-    @threads for i in 1:length(cleancode)
-        a[i]=parse(Int,cleancode[i],base=36)
-        b[i]=parse(Int,cleankey[i],base=36)
-        encoded[i]=abs(a[i]+b[i]+36)%36
-        encoded[i]-=10
-        if encoded[i]<0
-            encoded[i]+=36
-        end        
+    sdecode=Array{String}(undef,length(decoded))
+    @threads for i in 1:length(decoded)
+        @threads for j in 1:length(codex)
+            if codex[j,2]==decoded[i]
+                sdecode[i]=codex[j,1]
+                break
+            end
+        end
     end
-    c=Int.(encoded)
-    solution=Array{String}(undef,length(c))
-    @threads for i in 1:length(c)
-        solution[i]=string(c[i],base=36)
-    end
-    solution=join(solution)
-    solution
+    sdecode=join(sdecode)
 end
