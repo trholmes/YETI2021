584760d8c1a4f9883300aaa6c0a1050d98f518eb
Sanket Sharma <sanketsharma09@gmail.com>
2021-12-18 07:22:42 +0530

    Added code for day 5

--- Day_4/Day_4.py
+++ Day_4/Day_4.py
@@ -1,30 +1,3 @@
-class VigenereEncrypter:
-    def __init__(self, key, plain_text):
-        self.plain_text = plain_text
-        k = []
-        k.append(key)
-        if(len(plain_text) != len(key)):
-            for i in range(len(plain_text) - len(key)):
-                k.append(key[i%len(key)])
-        self.key = ''.join(k)
-
-        self.d = {}
-        count=0
-        for c in 'abcdefghijklmnopqrstuvwzyx1234567890':
-            self.d[c] = count;
-            count += 1 
-
-        self.inv_d = {v: k for k, v in self.d.items()}
-
-    def encrypt(self):
-        cipher_text = []
-        for i in range(len(self.plain_text)):
-            x = (self.d[self.plain_text[i]] + self.d[self.key[i]])%36
-            x += self.d['a']
-            cipher_text.append(self.inv_d[x])
-        return ''.join(cipher_text)
-        
-        
 class VigenereDecrypter:
     def __init__(self, key, encrypted_text):
         self.encrypted_text = encrypted_text
@@ -37,7 +10,7 @@ class VigenereDecrypter:
 
         self.d = {}
         count=0
-        for c in 'abcdefghijklmnopqrstuvwzyx1234567890':
+        for c in 'abcdefghijklmnopqrstuvwxyz1234567890':
             self.d[c] = count;
             count += 1 
 
@@ -53,12 +26,15 @@ class VigenereDecrypter:
         
 
 def main():
+    #The building address is 803 S Gay Street and the name is Bijou Theatre
     key = 'bijou'
-    with open('secretCode.txt') as f:
+    with open('secretCode.txt', 'r') as f:
         data = f.read()
 
     decrypter = VigenereDecrypter(key, data)
-    print(decrypter.decrypt())
+    plain_text = decrypter.decrypt()
+    with open('plainText.txt', 'w') as f1:
+        f1.write(plain_text)
 
 if __name__=='__main__':
     main()
---
+++ Day_5/Day5.py
@@ -0,0 +1,69 @@
+import struct
+from PIL import Image
+
+def fibonacci(n):
+    a,b = 0,1
+
+    if n == 1: 
+        return a
+    elif n == 2:
+        return b
+
+    for i in range(n-2):
+        tmp = a+b
+        a = b
+        b = tmp
+    return b
+
+def calculate_width():
+    evens,odds = 0,0
+    even_9th = -1
+    odd_6th = -1
+
+    i = 1
+    while True:
+        x = fibonacci(i)
+        if x%2 == 0:
+            evens += 1
+            if evens == 9:
+                even_9th = x
+        else: 
+            odds += 1 
+            if odds == 6:
+                odd_6th = x
+        if even_9th > - 1 and odd_6th > -1 : #This means both are set now
+            break
+        i += 1
+    return int(even_9th/odd_6th) 
+    
+def main():
+    n = calculate_width() #width of image
+    image_data = []
+    count = 0
+    #with open('../Day_4/plainText.txt', 'r') as f:
+    with open('/home/sanket/Downloads/hexdata.txt', 'r') as f:
+        while True:
+            block = ''
+            end = False
+            for i in range(6):
+                c = f.read(1)
+                count += 1
+                if not c:
+                    count -= 1 
+                    end = True
+                    break
+                block += c
+            if len(block) < 6:
+                count -= len(block)
+                break
+            if end:
+                break
+            image_data.append(struct.unpack('BBB', bytes.fromhex(block)))
+
+    length = int(count/n)
+    im = Image.new('RGB', (n, length))
+    im.putdata(image_data)
+    im.show()
+
+if __name__=='__main__':
+    main()
